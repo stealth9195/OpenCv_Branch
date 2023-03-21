645cefaa9543404181127b2e0e952e17bd304a6d
ShinDongHoon <satosi17@naver.com>
2023-03-21 13:25:05 +0900

    master 4

--- open1.py
+++ open1.py
@@ -1,11 +1,11 @@
 import cv2
 
 def hsv(src) :
-    dest = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-    return dest
+    dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    return dst2
 
 def binary(src):
     dst3 = src.copy()
     _, dst3 = cv2.threshold(dst3, 127, 255, cv2.THRESH_BINARY)
-    return dest
+    return dst3
     
